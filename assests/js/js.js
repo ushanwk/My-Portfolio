005607b5818ddc1ca0d6e2d07d17cacc33ec33f5
UshanKaushalya <Ushanwk22@gmail.com>
2023-06-24 22:19:27 +0530

    mobile response bud fix

---
+++ assests/js/js.js
@@ -0,0 +1,9 @@
+
+let loader = document.getElementById("loaderPage");
+//this is it. this will execute after the window is fully loaded
+window.addEventListener("load", function () {
+//console.log("This message will print just after fully loading the website")
+
+//hide the loader
+    loader.style.display = 'none';
+});
