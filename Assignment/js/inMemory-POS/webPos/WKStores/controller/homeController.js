cfa62597bef6fb61528bd45570e0198ee025d6cc
UshanKaushalya <Ushanwk22@gmail.com>
2023-06-25 22:02:53 +0530

    in memory pos order

--- Assignment/js/inMemory-POS/webPos/WKStores/controller/homeController.js
+++ Assignment/js/inMemory-POS/webPos/WKStores/controller/homeController.js
@@ -1,20 +1,32 @@
 document.getElementById("customerPage").style.display="none";
 document.getElementById("itemPage").style.display="none";
+document.getElementById("orderPage").style.display="none";
 
 document.getElementById("btnCustomer").addEventListener("click",function (){
     document.getElementById("homePage").style.display="none";
     document.getElementById("customerPage").style.display="block";
     document.getElementById("itemPage").style.display="none";
+    document.getElementById("orderPage").style.display="none";
 });
 
 document.getElementById("btnItem").addEventListener("click",function (){
     document.getElementById("homePage").style.display="none";
     document.getElementById("customerPage").style.display="none";
     document.getElementById("itemPage").style.display="block";
+    document.getElementById("orderPage").style.display="none";
 });
 
 document.getElementById("btnHome").addEventListener("click",function (){
     document.getElementById("homePage").style.display="block";
     document.getElementById("customerPage").style.display="none";
     document.getElementById("itemPage").style.display="none";
+    document.getElementById("orderPage").style.display="none";
+});
+
+
+document.getElementById("btnOrder").addEventListener("click",function (){
+    document.getElementById("homePage").style.display="none";
+    document.getElementById("customerPage").style.display="none";
+    document.getElementById("itemPage").style.display="none";
+    document.getElementById("orderPage").style.display="block";
 });
