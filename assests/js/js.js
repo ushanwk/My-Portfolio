b8384afe4429bf952ccaf21a0e16f2fb260642fe
UshanKaushalya <Ushanwk22@gmail.com>
2023-06-26 09:07:32 +0530

    loader bug fix

--- assests/js/js.js
+++ assests/js/js.js
@@ -1,9 +1,6 @@
 
 let loader = document.getElementById("loaderPage");
-//this is it. this will execute after the window is fully loaded
-window.addEventListener("load", function () {
-//console.log("This message will print just after fully loading the website")
 
-//hide the loader
+window.addEventListener("load", function () {
     loader.style.display = 'none';
 });
